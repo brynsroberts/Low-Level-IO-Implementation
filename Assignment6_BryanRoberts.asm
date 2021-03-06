c8b0e951cb0d80b13df128ce33a0e8aad991d40e
Bryan <bryan.roberts.home@gmail.com>
2021-03-06 08:51:05 -0800

    update format

--- Assignment6_BryanRoberts.asm
+++ Assignment6_BryanRoberts.asm
@@ -23,19 +23,19 @@ INCLUDE Irvine32.inc
 ; preconditions: stringSize stores capacity of userString
 ; registers changed: none
 ;----------------------------------------------------------------------------------------------------;
-getString			MACRO promptOffset, userString, stringCapacity, userStringLength 
+getString				MACRO promptOffset, userString, stringCapacity, userStringLength 
 
 	; save registers
-	push			edx
-	push			ecx
-	push			eax
+	push				edx
+	push				ecx
+	push				eax
 
 	; display prompt, accept user input
 	; save length of userString in userStringLength
-	displayString	promptOffset
+	displayString			promptOffset
 	mov				edx, userString
 	mov				ecx, stringCapacity
-	call			ReadString
+	call				ReadString
 	mov				userStringLength, eax
 
 	; reset registers
@@ -51,10 +51,10 @@ ENDM
 ; preconditions: stringOffset is address to null terminated string
 ; registers changed: none
 ;----------------------------------------------------------------------------------------------------;
-displayString		MACRO stringOffset
-	push			edx
+displayString				MACRO stringOffset
+	push				edx
 	mov				edx, stringOffset
-	call			WriteString
+	call				WriteString
 	pop				edx
 ENDM
 
@@ -66,77 +66,77 @@ STRINGSIZE = 32
 
 ; (insert variable definitions here)
 ; output strings
-introduction001		BYTE	"ASSIGNMENT 6: Designing low-level I/O procedures", 0
-introduction002		BYTE	"Written by: Bryan Roberts", 0
-introduction003		BYTE	"Please provide 10 signed decimal integers.", 0
-introduction004		BYTE	"Each number needs to be small enough to fit inside a 32 bit register.", 0
-introduction005		BYTE	"After you have finished inputting the raw numbers I will display a list", 0
-introduction006		BYTE	"of the integers, their sums, and their average value.", 0
-enterNumberPrompt	BYTE	"Please enter a signed number: ", 0
-errorPrompt			BYTE	"ERROR: You did not enter a signed number or your number was too big.", 0
-tryAgainPrompt		BYTE	"Please try again: ", 0
-displayPrompt		BYTE	"You entered the following numbers:", 0
-comma				BYTE	", ", 0
-sumPrompt			BYTE	"The sum of these numbers is: ",0
-averagePrompt		BYTE	"The rounded average is: ", 0
-farewellPrompt		BYTE	"Goodbye, and thanks for using my program!", 0
+introduction001				BYTE	"ASSIGNMENT 6: Designing low-level I/O procedures", 0
+introduction002				BYTE	"Written by: Bryan Roberts", 0
+introduction003				BYTE	"Please provide 10 signed decimal integers.", 0
+introduction004				BYTE	"Each number needs to be small enough to fit inside a 32 bit register.", 0
+introduction005				BYTE	"After you have finished inputting the raw numbers I will display a list", 0
+introduction006				BYTE	"of the integers, their sums, and their average value.", 0
+enterNumberPrompt			BYTE	"Please enter a signed number: ", 0
+errorPrompt				BYTE	"ERROR: You did not enter a signed number or your number was too big.", 0
+tryAgainPrompt				BYTE	"Please try again: ", 0
+displayPrompt				BYTE	"You entered the following numbers:", 0
+comma					BYTE	", ", 0
+sumPrompt				BYTE	"The sum of these numbers is: ",0
+averagePrompt				BYTE	"The rounded average is: ", 0
+farewellPrompt				BYTE	"Goodbye, and thanks for using my program!", 0
 
 ; arrays and variable strings
-array				DWORD	ARRAYSIZE DUP(0)
-userString			BYTE	STRINGSIZE DUP(0)
+array					DWORD	ARRAYSIZE DUP(0)
+userString				BYTE	STRINGSIZE DUP(0)
 
 ; variables
-userStringSize		DWORD	SIZEOF userString
-userStringLength	DWORD	?
-numSum				DWORD	?
+userStringSize				DWORD	SIZEOF userString
+userStringLength			DWORD	?
+numSum					DWORD	?
 sum					DWORD	?
-average				DWORD	?
+average					DWORD	?
 
 .code
 main PROC
 	; introduction
-	push			OFFSET introduction006
-	push			OFFSET introduction005
-	push			OFFSET introduction004
-	push			OFFSET introduction003
-	push			OFFSET introduction002
-	push			OFFSET introduction001
-	call			introduction
+	push				OFFSET introduction006
+	push				OFFSET introduction005
+	push				OFFSET introduction004
+	push				OFFSET introduction003
+	push				OFFSET introduction002
+	push				OFFSET introduction001
+	call				introduction
 	
 	; read in ARRAYSIZE vals and store in array after validating input
-	push			userStringLength
-	push			userStringSize
-	push			ARRAYSIZE
-	push			OFFSET userString
-	push			OFFSET array
-	push			OFFSET tryAgainPrompt
-	push			OFFSET errorPrompt
-	push			OFFSET enterNumberPrompt
-	call			readVal
+	push				userStringLength
+	push				userStringSize
+	push				ARRAYSIZE
+	push				OFFSET userString
+	push				OFFSET array
+	push				OFFSET tryAgainPrompt
+	push				OFFSET errorPrompt
+	push				OFFSET enterNumberPrompt
+	call				readVal
 
 	; display user entered numbers in array
-	push			numSum
-	push			OFFSET userString
-	push			OFFSET displayPrompt
-	push			OFFSET comma
-	push			ARRAYSIZE
-	push			OFFSET array
-	call			displayList
+	push				numSum
+	push				OFFSET userString
+	push				OFFSET displayPrompt
+	push				OFFSET comma
+	push				ARRAYSIZE
+	push				OFFSET array
+	call				displayList
 
 	; calculate and display sum and average of numbers stored in array
-	push			OFFSET average
-	push			OFFSET sum
-	push			numSum
-	push			OFFSET userString
-	push			OFFSET array
-	push			ARRAYSIZE
-	push			OFFSET averagePrompt
-	push			OFFSET sumPrompt
-	call			calculations
+	push				OFFSET average
+	push				OFFSET sum
+	push				numSum
+	push				OFFSET userString
+	push				OFFSET array
+	push				ARRAYSIZE
+	push				OFFSET averagePrompt
+	push				OFFSET sumPrompt
+	call				calculations
 
 	; farewll
-	push			OFFSET farewellPrompt
-	call			farewell
+	push				OFFSET farewellPrompt
+	call				farewell
 
 	exit	; exit to operating system
 main ENDP
@@ -151,36 +151,36 @@ main ENDP
 ; STACK FRAME STATE
 ; ebp					(ebp)		
 ; RET @					(ebp + 4)
-; introduction001 @		(ebp + 8)
-; introduction002 @		(ebp + 12)
-; introduction003 @		(ebp + 16)
-; introduction004 @		(ebp + 20)
-; introduction005 @		(ebp + 24)
-; introduction006 @		(ebp + 28)
+; introduction001 @			(ebp + 8)
+; introduction002 @			(ebp + 12)
+; introduction003 @			(ebp + 16)
+; introduction004 @			(ebp + 20)
+; introduction005 @			(ebp + 24)
+; introduction006 @			(ebp + 28)
 ;----------------------------------------------------------------------------------------------------;
 introduction PROC
-	push			ebp
+	push				ebp
 	mov				ebp, esp
 
-	displayString	[ebp + 8]					; value of introduction001
-	call			Crlf
+	displayString			[ebp + 8]			; value of introduction001
+	call				Crlf
 
-	displayString	[ebp + 12]					; value of introduction002
-	call			Crlf
-	call			Crlf
+	displayString			[ebp + 12]			; value of introduction002
+	call				Crlf
+	call				Crlf
 
-	displayString	[ebp + 16]					; value of introduction003
-	call			Crlf
+	displayString			[ebp + 16]			; value of introduction003
+	call				Crlf
 
-	displayString	[ebp + 20]					; value of introduction004
-	call			Crlf
+	displayString			[ebp + 20]			; value of introduction004
+	call				Crlf
 	
-	displayString	[ebp + 24]					; value of introduction005
-	call			Crlf
+	displayString			[ebp + 24]			; value of introduction005
+	call				Crlf
 
-	displayString	[ebp + 28]					; value of introduction006
-	call			Crlf
-	call			Crlf
+	displayString			[ebp + 28]			; value of introduction006
+	call				Crlf
+	call				Crlf
 
 	pop				ebp
 	ret				24
@@ -195,13 +195,13 @@ introduction ENDP
 ; STACK FRAME STATE
 ; ebp					(ebp)		
 ; RET @					(ebp + 4)
-; farewellPrompt @		(ebp + 8)
+; farewellPrompt @			(ebp + 8)
 ;----------------------------------------------------------------------------------------------------;
 farewell PROC
-	push			ebp
+	push				ebp
 	mov				ebp, esp
 
-	displayString	[ebp + 8]					; value of farewellPrompt
+	displayString			[ebp + 8]			; value of farewellPrompt
 
 	pop				ebp
 	ret				4
@@ -218,17 +218,17 @@ farewell ENDP
 ; STACK FRAME STATE
 ; ebp					(ebp)		
 ; RET @					(ebp + 4)
-; sumPrompt @			(ebp + 8)
-; averagePrompt @		(ebp + 12)
+; sumPrompt @				(ebp + 8)
+; averagePrompt @			(ebp + 12)
 ; ARRAYSIZE				(ebp + 16)
 ; array @				(ebp + 20)
-; userString @			(ebp + 24)
+; userString @				(ebp + 24)
 ; numSum				(ebp + 28)
 ; sum @					(ebp + 32)
 ; average @				(ebp + 36)
 ;----------------------------------------------------------------------------------------------------;
 calculations PROC
-	push			ebp
+	push				ebp
 	mov				ebp, esp
 	mov				edi, [ebp + 20]				; edi stores address of array
 	mov				ecx, [ebp + 16]				; ARRAYSIZE
@@ -239,38 +239,38 @@ sumArray:
 	mov				ebx, [edi]
 	add				eax, ebx
 	add				edi, 4
-	loop			sumArray
+	loop				sumArray
 
-	displayString	[ebp + 8]					; sumPrompt
+	displayString			[ebp + 8]				; sumPrompt
 
 	; call writeVal to turn sum into a string and display to user
 	mov				ebx, [ebp + 32]
 	mov				[ebx], eax
-	push			[ebp + 28]
-	push			[ebp + 24]
-	push			ebx
-	call			writeVal
-	call			Crlf
+	push				[ebp + 28]
+	push				[ebp + 24]
+	push				ebx
+	call				writeVal
+	call				Crlf
 
-	displayString	[ebp + 12]					; averagePrompt
+	displayString			[ebp + 12]				; averagePrompt
 
 	; divide calculated sum by ARRAYSIZE to get average
 	mov				edx, 0
 	cdq
 	mov				ecx, [ebp + 16]
-	idiv			ecx
+	idiv				ecx
 	mov				ebx, [ebp + 36]
 	mov				[ebx], eax
 	mov				[ebx], eax
 
 	; call writeVal to turn average into a string and display to user
-	push			[ebp + 28]
-	push			[ebp + 24]
-	push			ebx
-	call			writeVal
-;	call			WriteInt
-	call			Crlf
-	call			Crlf
+	push				[ebp + 28]
+	push				[ebp + 24]
+	push				ebx
+	call				writeVal
+;	call				WriteInt
+	call				Crlf
+	call				Crlf
 
 	pop				ebp
 	ret				32
@@ -285,18 +285,18 @@ calculations ENDP
 ; STACK FRAME STATE
 ; ebp					(ebp)
 ; RET @					(ebp + 20)
-; address of num @		(ebp + 24) 
-; userString @			(ebp + 28) 
+; address of num @			(ebp + 24) 
+; userString @				(ebp + 28) 
 ; numSum				(ebp + 32) 
 ;----------------------------------------------------------------------------------------------------;
 writeVal PROC
 
 	; save registers used in procedure
-	push			ebp
-	push			edi
-	push			ecx
-	push			ebx
-	push			eax
+	push				ebp
+	push				edi
+	push				ecx
+	push				ebx
+	push				eax
 	mov				ebp, esp
 	mov				edi, [ebp + 28]				; userString
 	mov				ebx, [ebp + 24]				; OFFSET of number to convert
@@ -308,7 +308,7 @@ writeVal PROC
 
 	; multiply by 1 to set sign flag, write correct sign into userString based on sign flag
 	mov				ebx, 1
-	imul			ebx							
+	imul				ebx							
 	js				negative
 	mov				eax, 43
 	jmp				firstNum
@@ -324,17 +324,17 @@ repeatDiv:
 	mov				ebx, [ebp + 24]				; OFFSET of number to convert
 	mov				eax, [ebx]					
 	mov				ebx, 1
-	imul			ebx							
+	imul				ebx							
 	jns				putInNumSum
 	mov				ebx, -1
-	imul			ebx
+	imul				ebx
 
 	; put original decimal value in num to start subracting from
 putInNumSum:
 	mov				[ebp + 32], eax				; numSum
 cont:
 	cdq
-	idiv			ecx
+	idiv				ecx
 	cmp				eax, 0
 	jne				finishDiv	
 	mov				ebx, 10
@@ -376,7 +376,7 @@ zeroNum:
 finishString:
 	mov				eax, 0
 	stosb
-	displayString	[ebp + 28]					; userString
+	displayString			[ebp + 28]				; userString
 
 	; reset registers
 	pop				eax
@@ -398,17 +398,17 @@ writeVal ENDP
 ; STACK FRAME STATE
 ; ebp					(ebp)		
 ; RET @					(ebp + 4)
-; enterNumberPrompt @	(ebp + 8)
-; errorPrompt @			(ebp + 12)
-; tryAgainPrompt @		(ebp + 16)
+; enterNumberPrompt @			(ebp + 8)
+; errorPrompt @				(ebp + 12)
+; tryAgainPrompt @			(ebp + 16)
 ; array @				(ebp + 20)
-; userString @			(ebp + 24)
+; userString @				(ebp + 24)
 ; ARRAYSIZE				(ebp + 28)
-; userStringSize		(ebp + 32)
-; userStringLength		(ebp + 36)
+; userStringSize			(ebp + 32)
+; userStringLength			(ebp + 36)
 ;----------------------------------------------------------------------------------------------------;
 readVal PROC
-	push			ebp
+	push				ebp
 	mov				ebp, esp
 	mov				edi, [ebp + 20]				; array		
 	mov				ecx, [ebp + 28]				; ARRAYSIZE
@@ -419,20 +419,20 @@ readVal PROC
 fillArray:
 	cmp				eax, 0
 	jne				badPrompt
-	getString		[ebp + 8], [ebp + 24], [ebp + 32], [ebp + 36]
+	getString			[ebp + 8], [ebp + 24], [ebp + 32], [ebp + 36]
 	jmp				afterPrompt
 badPrompt:
-	displayString	[ebp + 12]
-	Call			Crlf
-	getString		[ebp + 16], [ebp + 24], [ebp + 32], [ebp + 36]
+	displayString			[ebp + 12]
+	Call				Crlf
+	getString			[ebp + 16], [ebp + 24], [ebp + 32], [ebp + 36]
 
 	; we now have unvalidated userString, setup for looping through each character, validating
 	; and turning string in decimal
 afterPrompt:
-	push			ecx
+	push				ecx
 	mov				ecx, [ebp + 36]				; userStringLength
 	mov				esi, [ebp + 24]				; userString
-	mov				ebx, 0						; starter value
+	mov				ebx, 0					; starter value
 	cld								
 charLoop:
 	mov				eax, 0
@@ -452,11 +452,11 @@ charLoop:
 characterFound:
 	cmp				ecx, [ebp + 36]				; check to see if we are on first character
 	jne				badInput
-	cmp				eax, 43						; compare to + sign
+	cmp				eax, 43					; compare to + sign
 	je				signFound
-	cmp				eax, 45						; compare to - sign
+	cmp				eax, 45					; compare to - sign
 	je				signFound
-	jmp				badInput					; invalid character found, jump to badInput
+	jmp				badInput				; invalid character found, jump to badInput
 signFound:	
 	dec				ecx
 	jmp				charLoop
@@ -481,7 +481,7 @@ startCalculation:
 	je				skipMult
 
 	; if mul results in change of edx value, number is too large for 32-bit register
-	push			ebx
+	push				ebx
 	mov				ebx, 10
 	mov				edx, 0
 	mul				ebx
@@ -501,7 +501,7 @@ skipMult:
  	cmp				eax, 45
 	jne				putInArray
 	mov				eax, -1
-	imul			ebx
+	imul				ebx
 	mov				ebx, eax
 
 	; put decimal value in next position of array
@@ -531,41 +531,41 @@ readVal ENDP
 ; array @				(ebp + 8)
 ; ARRAYSIZE				(ebp + 12)
 ; comma @				(ebp + 16)
-; displayPrompt	@		(ebp + 20)
-; userString @			(ebp + 24)
+; displayPrompt	@			(ebp + 20)
+; userString @				(ebp + 24)
 ; numSum				(ebp + 28)
 ;----------------------------------------------------------------------------------------------------;
 displayList PROC	
-	push			ebp
+	push				ebp
 	mov				ebp, esp
 	mov				ecx, [ebp + 12]				; ARRAYSIZE
 	mov				edi, [ebp + 8]				; array
 
 	; displayPrompt
-	call			Crlf
-	displayString	[ebp + 20]
-	call			Crlf
+	call				Crlf
+	displayString			[ebp + 20]
+	call				Crlf
 
 	; loop through array, convert number to string and display to console
 nextElement:
 	mov				eax, [edi]
-	push			[ebp + 28]
-	push			[ebp + 24]
-	push			edi
-	call			writeVal
+	push				[ebp + 28]
+	push				[ebp + 24]
+	push				edi
+	call				writeVal
 	add				edi, 4
 	cmp				ecx, 1
  	je				skip
 
 	; write ", " to console
 	mov				edx, [ebp + 16]
-	call			WriteString
+	call				WriteString
 
 	; don't add comma if on last number
 skip:
-	loop			nextElement
-	call			Crlf
-	call			Crlf
+	loop				nextElement
+	call				Crlf
+	call				Crlf
 
 	pop				ebp
 	ret				24
